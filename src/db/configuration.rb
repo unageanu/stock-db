f6ec52e932dee59999cec73c3cb82f1a58c13884
unageanu <masaya.yamauchi@gmail.com>
2016-03-22 12:14:09 +0900

    Create a database if database is not exists.

--- src/db/configuration.rb
+++ src/db/configuration.rb
@@ -4,11 +4,19 @@ require 'config/load_env'
 require "active_record"
 
 #ActiveRecord::Base.logger = Logger.new($stdout)
-ActiveRecord::Base.establish_connection(
+config = {
   adapter:  "postgresql",
   host:     ENV['POSTGRES_HOST'] || 'localhost',
   database: "stockdb",
   port:     ENV['POSTGRES_PORT'] || 5432,
   username: ENV['POSTGRES_USER'] || 'postgres',
   password: ENV['POSTGRES_PASSWORD'] || 'mysecretpassword'
-)
+}
+begin
+  ActiveRecord::Base.establish_connection(config)
+  ActiveRecord::Base.connection
+rescue ActiveRecord::NoDatabaseError
+  ActiveRecord::Base.establish_connection(config.merge(database:'postgres'))
+  ActiveRecord::Base.connection.create_database(config[:database])
+  ActiveRecord::Base.establish_connection(config)
+end
