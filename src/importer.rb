8c5c523b435f1595f9100864f81b6fe463745da8
unageanu <masaya.yamauchi@gmail.com>
2016-03-22 09:50:28 +0900

    Modify rates validation.

--- src/importer.rb
+++ src/importer.rb
@@ -1,6 +1,6 @@
 # coding: utf-8
 
-require "thread"
+require 'thread/pool'
 require "db/configuration"
 require "db/migrate/table_builder"
 require "models/stock"
@@ -65,15 +65,21 @@ module StockDB
     end
 
     def find_or_create_rate(stock_id, rate_info)
-      return unless rate_info['open']
+      return unless valid_rate?(rate_info)
       attributes = { stock_id:stock_id, date: rate_info['date'] }
       Rate.find_or_create_by(attributes) do |rate|
         rate.open   = rate_info['open']
         rate.close  = rate_info['close']
         rate.high   = rate_info['high']
         rate.low    = rate_info['low']
-        rate.volume = rate_info['volume'].to_i
+        rate.volume = rate_info['volume'].to_i || 0
       end
+    end
+
+    def valid_rate?(rate_info)
+      rate_info['open'] && rate_info['close'] \
+        && rate_info['high'] && rate_info['low'] \
+        && rate_info['date']
     end
 
   end
